5e4bdd1a813cc967344646ebc8a0ded4b28f4683
wangbo-lenovo <wangbo_dream@163.com>
2020-03-24 15:20:11 +0800

    新建oneboot

---
+++ oneboot/src/main/java/com/xjtu/oneboot/OnebootApplication.java
@@ -0,0 +1,13 @@
+package com.xjtu.oneboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnebootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnebootApplication.class, args);
+    }
+
+}
---
+++ oneboot/src/test/java/com/xjtu/oneboot/OnebootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xjtu.oneboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnebootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
